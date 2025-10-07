9206116306dc7452d3175f79e0656cdf9089341b
aliamr <aliamasrukha@gmail.com>
2025-10-07 08:08:33 +0700

    inventoryPertanian

--- inventoryPertanian/src/frame/dashboard.java
+++ inventoryPertanian/src/frame/dashboard.java
@@ -8,12 +8,12 @@ package frame;
  *
  * @author user
  */
-public class barangKeluar extends javax.swing.JFrame {
+public class dashboard extends javax.swing.JFrame {
 
     /**
-     * Creates new form barangKeluar
+     * Creates new form dashboard
      */
-    public barangKeluar() {
+    public dashboard() {
         initComponents();
     }
 
@@ -32,11 +32,11 @@ public class barangKeluar extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1300, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 650, Short.MAX_VALUE)
         );
 
         pack();
@@ -59,20 +59,20 @@ public class barangKeluar extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(barangKeluar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(barangKeluar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(barangKeluar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(barangKeluar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(dashboard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new barangKeluar().setVisible(true);
+                new dashboard().setVisible(true);
             }
         });
     }
